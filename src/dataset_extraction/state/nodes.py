56fa9842573e4d641078a9473f06628814ac20ce
passionfruit03 <jhihyih@andrew.cmu.edu>
2026-06-12 15:45:40 +0800

    refactor: move UsageNode and UsageNodes into usage/ submodule. fix: change UsageNodes add function back to upsert instead of throwing an error if key exists.

--- src/dataset_extraction/state/nodes.py
+++ src/dataset_extraction/state/nodes.py
@@ -3,7 +3,6 @@ import uuid
 from pydantic import model_validator
 
 from dataset_extraction.dataset.datasets import Dataset
-from dataset_extraction.usage.usages import DatasetUsage
 
 _DATASET_NS = uuid.UUID("b4e9a3c1-5d7f-4e2b-8a6c-3f1d9e0b2a4c")
 
@@ -20,7 +19,3 @@ class DatasetNode(Dataset):
             key = f"{self.paper_title or ''}::{self.name}"
             self.dataset_id = str(uuid.uuid5(_DATASET_NS, key))
         return self
-
-
-class UsageNode(DatasetUsage):
-    paper_title: str  # paper that used this dataset
