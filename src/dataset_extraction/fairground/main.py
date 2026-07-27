61a55b09b58f7deba11094a4c584bbbd4c4626c5
passionfruit03 <jhihyih@andrew.cmu.edu>
2026-07-27 14:59:54 +0800

    refactor: refactor working directory structure

--- src/dataset_extraction/fairground/main.py
+++ src/dataset_extraction/fairground/main.py
@@ -237,7 +237,7 @@ def main() -> None:
     args = parser.parse_args()
 
     working_dir = Path(args.working_dir)
-    setup_logging(working_dir / "fg" / "logs")
+    setup_logging(working_dir / "logs")
 
     kwargs = {"model": args.model}
     if args.reasoning_effort is not None:
--- src/dataset_extraction/fairground/map_usages.py
+++ src/dataset_extraction/fairground/map_usages.py
@@ -124,7 +124,7 @@ def main() -> None:
     args = parser.parse_args()
 
     working_dir = Path(args.working_dir)
-    setup_logging(working_dir / "fg" / "logs")
+    setup_logging(working_dir / "logs")
     run(working_dir, model=args.model)
 
 
